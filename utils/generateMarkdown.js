06dd987d79a3e07771d459d5149f766140522c12
louiskgkim <lkgk31@gmail.com>
2023-01-11 18:54:50 -0800

    added finished markdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,79 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return `
+  ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+    return `
+  [${license}](https://choosealicense.com/licenses/${license})
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+    return `
+  ## [License](#table-of-contents)
+  The application is covered under the following license:
+  ${renderLicenseLink(license)}
+    `;
+  } else {
+    return ' ';
+  }
+}
+
+// Added a function that returns license in table of contents
+// If there is no license it should return an empty string
+ function renderLicenseTOC(license) {
+  if (license !== 'no license') {
+  return `
+  * [License](#license)
+    `;
+  } else {
+    return ' ';
+  }
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  # ${data.title}
+  
+  ${renderLicenseBadge(data.license)}
+  ## Table-of-Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseTOC(data.license)}
+  * [Contributing](#contributing)
+  * [Questions](#questions)
+  
+  ## [Description](#table-of-contents)
+  ${data.what}
+  ${data.why}
+  ${data.how}
+  ## [Installation](#table-of-contents)
+  ${data.installation}
+  ## [Usage](#table-of-contents)
+  ${data.usage}
+  
+  To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. 
+  Then, using the relative filepath, add it to your README.
+  ![alt text](assets/images/screenshot.png)
 `;
 }
 
