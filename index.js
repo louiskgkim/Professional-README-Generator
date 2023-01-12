c023168d0808ba33609826c419def8c5f4bde1de
louiskgkim <lkgk31@gmail.com>
2023-01-11 18:53:54 -0800

    added finished index file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
+// fs is a Node standard library package for reading and writing files
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
